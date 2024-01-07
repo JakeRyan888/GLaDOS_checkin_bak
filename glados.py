07f68eda33553787d3cd1ab8d794d3222b9d1e7a
JakeRyan888 <69430204+JakeRyan888@users.noreply.github.com>
2024-01-07 13:12:01 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -22,19 +22,16 @@ if __name__ == '__main__':
     payload = {
         'token': 'glados.one'
     }
-    days = []
-    balance = []
-    change = []
     for cookie in cookies:
         checkin = requests.post(checkin_url,
                                 headers={'cookie':cookie,'referrer':referrer,'origin':origin,'user-agent':useragent,'content-type':'application/json; charset=utf-8'},data = json.dumps(payload))
         status = requests.get(status_url,headers={'cookie':cookie,'referrer':referrer,'origin':origin,'useragent':useragent})
         
-        days = status.json()['data']['leftDays'].split('.')[0]
+        days = str(status.json()['data']['leftDays']).split('.')[0]
         email = status.json()['data']['email']
 
-        balance = checkin.json()['list'][0]['balance'].split('.')[0]
-        change = checkin.json()['list'][0]['change'].split('.')[0]
+        balance = str(checkin.json()['list'][0]['balance']).split('.')[0]
+        change = str(checkin.json()['list'][0]['change']).split('.')[0]
 
         if 'message' in checkin.text:
             msg = checkin.json()['message']
