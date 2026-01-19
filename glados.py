62d4d5bc80de9ff02bff5dc2211c4ab230eb6b8f
JakeRyan888 <69430204+JakeRyan888@users.noreply.github.com>
2026-01-19 14:33:22 +0800

    Update glados.py

--- glados.py
+++ glados.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     origin = 'https://glados.cloud'
     useragent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     payload = {
-        'token': 'glados.one'
+        'token': 'glados.cloud'
     }
     for cookie in cookies:
         checkin = requests.post(checkin_url,
